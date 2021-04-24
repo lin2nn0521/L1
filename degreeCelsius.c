d2500439f008a6dc2bb1cc54177ed2b463ee7a89
lin2nn0521 <83106226+lin2nn0521@users.noreply.github.com>
2021-04-24 15:42:00 +0800

    L1-攝氏與華氏
    攝氏轉換華氏

---
+++ degreeCelsius.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int degreeC ;
+    double degreeF  ;
+    degreeC=128;
+    degreeF=degreeC*9/5+32;
+    printf("Äá¤ó %d «× ",degreeC);
+    printf("=");
+    printf("µØ¤ó %f «× ",degreeF);
+    return 0;
+}
