3d18802d79f1be342c44b86d25307187db706cdc
lin2nn0521 <83106226+lin2nn0521@users.noreply.github.com>
2021-04-24 13:17:30 +0800

    L1-三角形面積
    三角形面積計算

---
+++ L1-triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   float x , h;
+   x=25.6;
+   h=10.84;
+   printf("三角形的底為 %f " , x);
+   printf("高為%f，", h);
+   printf("面積等於 %f", x*h/2);
+   return 0;
+}
